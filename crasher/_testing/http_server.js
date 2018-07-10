f21596b3f5ed12dd1a0d6050c532fedf834fc956
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-07-10 23:00:26 +0200

    Node.js http server for testing attacks

---
+++ crasher/_testing/http_server.js
@@ -0,0 +1,29 @@
+﻿var http = require('http');
+var connections = 0;
+var seconds = 0;
+
+console.log("Server started..");
+
+//Connection status manager (timer every second)
+var secondCounter = setInterval(function () {
+    console.clear();
+    if (connections > 0) {
+        seconds++;
+        WriteStatus();
+    } else {
+        console.log("No requests.. YET!!");
+    }
+}, 1000);
+
+//create a server object:
+http.createServer(function (request, response) {
+    //2 requests: 1 for the page, 1 for the favicon
+    connections++;
+}).listen(80); //the server object listens on port 8080
+
+//Requests per second counter
+function WriteStatus() {
+    var requestsPerSecond = connections / seconds;
+    console.log("Total requests: " + connections);
+    console.log(requestsPerSecond + " requests/s");
+}
