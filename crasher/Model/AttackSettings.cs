1724518c990c8042d6db4f5d03d611da26c0e4cc
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-07-10 23:03:53 +0200

    Class that contains attacks settings

--- crasher/Model/AttackSettings.cs
+++ crasher/Model/AttackSettings.cs
@@ -10,7 +10,7 @@ namespace crasher.Model
     {
         public static string Url { get; set; } = "none";
         public static AttackType Mode { get; set; } = AttackType.ClientRequest;
-        public static bool Threading { get; set; } = true;
+        public static bool Threading { get; set; } = false;
         public static int Threads { get; set; } = 10;
     }
 }
