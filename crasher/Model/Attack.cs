1cefcb6d59016451b9a241bcb68479aca360350f
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-08-20 14:16:28 +0200

    Added more constructors for better usability

--- crasher/Model/Attack.cs
+++ crasher/Model/Attack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using crasher.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,21 @@ namespace crasher.Model
             AttackSettings.Mode = mode;
             AttackSettings.Url = url;
         }
+
+        public Attack(string url, string mode, int time)
+        {
+            AttackSettings.Mode = AttackHelper.StringToAttackType(mode);
+            AttackSettings.Url = url;
+            AttackSettings.Time = time;
+        }
+
+        public Attack(string url, AttackType mode, int time)
+        {
+            AttackSettings.Mode = mode;
+            AttackSettings.Url = url;
+            AttackSettings.Time = time;
+        }
+
 
         /**
          * Metodo che starta l'attacco desiderato
