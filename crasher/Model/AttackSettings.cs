0bbeeb0b48b62dabbb2251edff5b18fadd00cdc2
Luca Di Bello <luca.dibello@samtrevano.ch>
2018-08-20 14:16:55 +0200

    Added time to AttackSettings class

--- crasher/Model/AttackSettings.cs
+++ crasher/Model/AttackSettings.cs
@@ -10,6 +10,8 @@ namespace crasher.Model
     {
         public static string Url { get; set; } = "none";
         public static AttackType Mode { get; set; } = AttackType.HttpFlood;
+        public static int Time { get; set; } = 60; //seconds
+
         public static bool Threading { get; set; } = false;
         public static int Threads { get; set; } = 10;
         public static int PacketSize { get; set; } = 2048;
